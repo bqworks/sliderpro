165a2ce61deff418469bcc2ffeb1439d64a06b08
davidghi <david.ghiurau@gmail.com>
2015-10-02 22:33:08 +0300

    Revert "update public assets"
    This reverts commit 5c6b114c678d6dc792ab28135b1bf453ccdff483.

--- public/assets/js/jquery.sliderPro.js
+++ public/assets/js/jquery.sliderPro.js
@@ -1,5 +1,5 @@
 /*!
-*  - v1.2.2
+*  - v1.2.1
 * Homepage: http://bqworks.com/slider-pro/
 * Author: bqworks
 * Author URL: http://bqworks.com/
@@ -900,9 +900,6 @@
 			// Indicates if the image will be centered
 			centerImage: true,
 
-			// Indicates if the image can be scaled up more than its original size
-			allowScaleUp: true,
-
 			// Indicates if height of the slider will be adjusted to the
 			// height of the selected slide
 			autoHeight: false,
@@ -1154,18 +1151,9 @@
 				return;
 			}
 
-			if ( this.settings.allowScaleUp === false ) {
-				this.$mainImage.css({ 'width': '', 'height': '', 'maxWidth': '', 'maxHeight': '' });
-
-				var naturalWidth = this.$mainImage.width(),
-					naturalHeight = this.$mainImage.height();
-
-				this.$mainImage.css({ 'maxWidth': naturalWidth, 'maxHeight': naturalHeight });
-			}
-
 			// After the main image has loaded, resize it
 			if ( this.settings.autoHeight === true ) {
-				this.$mainImage.css({ width: '100%', height: 'auto' });
+				this.$mainImage.css({ width: '100%', height: 'auto', 'marginLeft': '', 'marginTop': '' });
 			} else {
 				if ( this.settings.imageScaleMode === 'cover' ) {
 					if ( this.$mainImage.width() / this.$mainImage.height() <= this.width / this.height ) {
@@ -1182,10 +1170,10 @@
 				} else if ( this.settings.imageScaleMode === 'exact' ) {
 					this.$mainImage.css({ width: '100%', height: '100%' });
 				}
-			}
 
-			if ( this.settings.centerImage === true ) {
-				this.$mainImage.css({ 'marginLeft': ( this.$imageContainer.width() - this.$mainImage.width() ) * 0.5, 'marginTop': ( this.$imageContainer.height() - this.$mainImage.height() ) * 0.5 });
+				if ( this.settings.centerImage === true ) {
+					this.$mainImage.css({ 'marginLeft': ( this.$imageContainer.width() - this.$mainImage.width() ) * 0.5, 'marginTop': ( this.$imageContainer.height() - this.$mainImage.height() ) * 0.5 });
+				}
 			}
 		},
 
@@ -2751,9 +2739,6 @@
 					that.gotoSlide = that._layersGotoSlide;
 				}, 1 );
 			}
-
-			// Show the layers for the initial slide
-			this.showLayers( this.selectedSlideIndex );
 		},
 
 		// When the slider resizes, try to scale down the layers proportionally. The automatic scaling
@@ -2767,6 +2752,9 @@
 				scaleRatio;
 
 			if ( this.settings.autoScaleLayers === false ) {
+				// Show the layers for the initial slide
+				this.showLayers( this.selectedSlideIndex );
+				
 				return;
 			}
 
@@ -2797,6 +2785,9 @@
 					});
 				}
 			});
+
+			// Show the layers for the initial slide
+			this.showLayers( this.selectedSlideIndex );
 		},
 
 		// Replace the 'gotoSlide' method with this one, which makes it possible to 
